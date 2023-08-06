2754c2968b743f0264e21ad49891f8e59da84c7e
dsaragih <dabestender@gmail.com>
2023-08-05 20:10:06 -0400

    change to train script

--- train.sh
+++ train.sh
@@ -24,8 +24,9 @@ python unet_plusplus.py train \
     --num_epochs 150 \
     --device_id 0  \
     --mode "full_syn_train" \
-    --img_dir "/root/diffusion-gen/guided_diffusion/segmented-images" \
-    --test_dir "/root/divergent-nets/data/data_files/test-images" \
-    --pkl_path "/root/divergent-nets/data/data_files/GAN_dict.pkl" \
+    --img_dir "/home/daniel/diffusion-gen/guided_diffusion/segmented-images" \
+    --pkl_path "/home/daniel/divergent-nets/data/data_files/cc_styled_samples_dil.pkl" \
+    --test_dir "/home/daniel/divergent-nets/data/data_files/test-images" \
+    --n_samples 1 \
     --out_dir ./outputs/ \
     --tensorboard_dir ./outputs/ 
